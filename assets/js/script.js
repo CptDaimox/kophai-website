5186dddec198cc6b949f4fd17cfe9891d870d327
CptDaimox <toan.ttqdev@gmail.com>
2024-12-22 17:40:14 +0100

    feat: update holiday banner logic and background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-// document.getElementById("closeBanner").addEventListener("click", function () {
-//   document.getElementById("holidayBanner").style.display = "none";
-// });
+window.onload = function () {
+  document.body.classList.remove("is-preload");
+  const banner = document.getElementById("holidayBanner");
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const currentMonth = currentDate.getMonth() + 1;
+  const bg = document.getElementById("bg");
+
+  if(currentMonth !== 12) {
+    banner.style.display = "none";
+  } else if (currentDay >= 15 && currentDay <= 31) {
+    banner.style.display = "";
+    bg.style.backgroundImage = "url('assets/css/images/xmas-bg.png')";
+  }
+};
+window.ontouchmove = function () {
+  return false;
+};
